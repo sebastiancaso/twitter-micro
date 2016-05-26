dfffbb48a7de0f6539bf6b3f2fecfc8c24005df0
sebastiancaso <sebcaso@gmail.com>
2016-05-25 22:13:32 -0400

    twitter initializer added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+	skip_before_action :verify_authenticity_token, only: [:index]
+
+	def index 
+		# might want to initialize client variable in controller to avoid having to set variable globally
+		@tweet =  $client.search("#healthcare -rt", lang: "en").take(10)
+	end
 end
