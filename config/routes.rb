c469ab0bd93a9fb48b2dc50f08866d24311ce680
sebastiancaso <sebcaso@gmail.com>
2016-05-26 19:32:24 -0400

    add route to provide tweets

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
+  post '/' => "welcome#index", as: "root", defaults: {format: :json}
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
